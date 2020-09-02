973edf3f4eed701d2d6c04b7d974b091fb822c25
Kylin <wangds1@yonyou.com>
2020-09-02 16:51:33 +0800

    这是一个刚刚搭建起来的spring cloud父子项目，父项目用于控制spring boot等版本、统一管理，子项目增加了实体以及API的公共包，内容未填充，未来会慢慢的完善

---
+++ kylin-secret-cloud/server-entity/src/main/java/wang/kylinking/ServerEntityApplication.java
@@ -0,0 +1,13 @@
+package wang.kylinking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerEntityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerEntityApplication.class, args);
+    }
+
+}
---
+++ kylin-secret-cloud/server-entity/src/test/java/wang/kylinking/ServerEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package wang.kylinking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerEntityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
