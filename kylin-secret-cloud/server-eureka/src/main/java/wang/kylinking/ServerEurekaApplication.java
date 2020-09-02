720cbe7bb505b06c449077300db81d47b029031c
Kylin <wangds1@yonyou.com>
2020-09-02 15:58:29 +0800

    这是一个刚刚搭建起来的spring cloud父子项目，父项目用于控制spring boot等版本、统一管理，子项目目前仅有一个做了简单配置的Eureka，未来会慢慢的完善

---
+++ kylin-secret-cloud/server-eureka/src/main/java/wang/kylinking/ServerEurekaApplication.java
@@ -0,0 +1,18 @@
+package wang.kylinking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Kylin
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class ServerEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerEurekaApplication.class, args);
+    }
+
+}
